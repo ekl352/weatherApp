562afb2137a456e0bd6a04970f4840dcfeeb8d95
Mohga <38896263+MohgaEmam@users.noreply.github.com>
2020-07-06 17:59:07 +0200

    Update app.js

--- Desktop/Udacity/Javascript/WeatherJournal/website/app.js
+++ Desktop/Udacity/Javascript/WeatherJournal/website/app.js
@@ -1,70 +1,77 @@
 /* Global Variables */
 
-let baseURL = 'http://api.openweathermap.org/data/2.5/forecast/weather?'
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
-
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
 
 let apiKey = '&appid=702b2d5deaf24a831d6750b83fcdfb4c';
 
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-// Event listener to add function to existing HTML DOM element
 
 document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener */
 function performAction(e){
-	const newZip = document.getElementById('zip').value;
+	let zip = document.getElementById('zip').value;
+    let content = document.getElementById('feelings').value;
+    getWeather(baseURL, zip, apiKey)
+   .then(function(data){
+      console.log('data checkin ', data)
+      let temp = data.main.temp;
+      postData('/addData', {temp:temp, content:content, date:newDate})
+     })
 
-	getWeather(baseURL, newZip, apiKey)
-
-	.then(function(data){
-		console.log(data)
-		postData('/addData', {date: data.date, temp: data.temp, content: data.content} );
-	});
-
-	updateUI()
+	 .then(function(){
+		updateUI()
+	  }
+	   );
 
 };
 
 /* Function to GET Web API Data*/
 
-const getWeather = async(baseURL, newZip, apiKey)=>{
-	const res = await fetch(baseURL+newZip+apiKey);
-	try {
-		const data = await res.json();
-		console.log(data);
-		postData('addData', data);
-	} catch (error) {
-		console.log("error", error);
-	}
+const getWeather = async (baseURL, zip, key)=>{
+    // Use the fetch API to retrieve the current weather data for the users zip code
+    const res = await fetch(baseURL+zip+key)
+    try {
+
+      const data = await res.json();
+      return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
 }
 
 /* Function to POST data */
 
 const postData = async ( url = '', data = {})=>{
-	console.log(data)
-	  const response = await fetch(url, {
-	  method: 'POST',
-	  credentials: 'same-origin',
-	  headers: {
-	  	'Content-Type': 'application/json',
-	  },
-	  body: JSON.stringify(data),
 
-	});
+	console.log('data ',data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    // mode: 'cors', // no-cors, cors, *same-origin
+    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
 
-	  try {
-	  	const newData = await response.json();
-	  	console.log(newData);
-	  	return newData
-	  } catch (error) {
-	  	console.log("error", error);
-	  }
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+      console.log("newData ", newData);
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
 }
 
 
@@ -72,16 +79,11 @@ const updateUI = async () => {
 	const request = await fetch ('/all');
 	try {
 		const allData = await request.json();
-		console.log(allData);
-		document.getElementById('date').innerHTML = allData[0].date;
-		document.getElementById('temp').innerHTML = allData[0].temp;
-		document.getElementById('content').innerHTML = allData[0].content;
+		console.log('all data: ',allData);
+		document.getElementById('date').innerHTML = allData.date;
+		document.getElementById('temp').innerHTML = allData.temp;
+		document.getElementById('content').innerHTML = allData.content;
 	} catch(error){
 		console.log("error", error);
 	}
 }
-
-
-
-
-
